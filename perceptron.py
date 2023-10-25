c459ff1ad6d4dbd212469bc1391ec1eafad3c683
sehbeygi79 <sehbeygi79@gmail.com>
2023-10-25 21:30:15 +0330

    switched to OOD, do_preprocessing got a bit cleaner, evaluation added

--- perceptron.py
+++ perceptron.py
@@ -2,66 +2,64 @@ import numpy as np
 import pandas as pd
 
 
-def do_preprocessings(file_path):
-    df = pd.read_csv(file_path)
+class Perceptron:
+    def __init__(self, file_path, seed=37):
+        df = self.__do_preprocessings(pd.read_csv(file_path))
 
-    # shuffle
-    # df = df.sample(frac=1).reset_index(drop=True)
-    # remove corrupted data
-    df = df.loc[(df['BMI'] != 0) & (df['Glucose'] != 0)
-                & (df['BloodPressure'] != 0)]
-    x_df = df.loc[:, df.columns != 'Outcome']
-    y_df = df['Outcome']
-    y_df.replace(to_replace={True: 1, False: -1}, inplace=True)
-    # normalize
-    x_df = (x_df-x_df.min())/(x_df.max()-x_df.min())
+        self.train_df = df.sample(frac=0.85, random_state=seed)
+        test_df = df.drop(self.train_df.index)
+        self.test_x = test_df.loc[:, test_df.columns != 'Outcome'].values
+        self.test_y = test_df['Outcome'].values.reshape(-1, 1)
 
-    df.loc[:, df.columns != 'Outcome'] = x_df
-    df['Outcome'] = y_df
-    return df
+    def __do_preprocessings(self, df):
+        # shuffle
+        # df = df.sample(frac=1).reset_index(drop=True)
+        # remove corrupted data
+        df = df.loc[(df['BMI'] != 0) & (df['Glucose'] != 0)
+                    & (df['BloodPressure'] != 0)]
+        x_df = df.loc[:, df.columns != 'Outcome']
+        y_df = df.loc[:, df.columns == 'Outcome'].replace(
+            to_replace={True: 1, False: -1})
+        # normalize
+        x_df = (x_df-x_df.min())/(x_df.max()-x_df.min())
+
+        return pd.concat([x_df, y_df], axis=1, join='inner')
+
+    def train(self, lr=1, epoch=1, evaluate=False):
+        x_df = self.train_df.loc[:, self.train_df.columns != 'Outcome']
+        y_df = self.train_df['Outcome']
+
+        d = x_df.shape[1]
+        w = np.zeros((d+1, 1))
+
+        for i in range(epoch):
+            for index, row in x_df.iterrows():
+                x = np.insert(np.array(row), 0, 1).reshape(-1, 1)
+                pred = np.sign(np.dot(x.T, w))
+                y = y_df.loc[index]
+                if(pred != y):
+                    w += lr*y*x
+                    if evaluate:
+                        test_y_hat = self.__predict(self.test_x, w)
+                        print(self.__calc_acc(self.test_y, test_y_hat))
+
+        return w
+
+    def __predict(self, X, w):
+        # todo: assertion for w.shape == (x_df.shape[1]+1, 1)
+        ones_column = np.ones((X.shape[0], 1))
+        X = np.hstack((ones_column, X))
+        return np.sign(np.dot(X, w))
+
+    def __calc_acc(self, y, y_hat):
+        correct_preds = np.sum(y == y_hat)
+        return (correct_preds / len(y)) * 100
 
 
-def train(train_df, lr=1, epoch=1):
-    x_df = train_df.loc[:, train_df.columns != 'Outcome']
-    y_df = train_df['Outcome']
-
-    d = x_df.shape[1]
-    w = np.zeros((d+1, 1))
-    
-    for i in range(epoch):
-        for index, row in x_df.iterrows():
-            x = np.insert(np.array(row), 0, 1).reshape(-1, 1)
-            pred = np.sign(np.dot(x.T, w))
-            y = y_df.loc[index]
-            if(pred != y):
-                w += lr*y*x
-
-    return w
+def main():
+    perceptron = Perceptron('Dataset.csv')
+    weights = perceptron.train(epoch=1, evaluate=True)
 
 
-def predict(X, w):
-    # todo: assertion for w.shape == (x_df.shape[1]+1, 1)
-    ones_column = np.ones((X.shape[0], 1))
-    X = np.hstack((ones_column, X))
-    return np.sign(np.dot(X, w))
-
-
-def calc_acc(y, y_hat):
-    correct_preds = np.sum(y == y_hat)
-    return (correct_preds / len(y)) * 100
-    
-
-
-file_path = 'Dataset.csv'
-df = do_preprocessings(file_path)
-
-seed = 37
-train_df = df.sample(frac=0.85, random_state=seed)
-test_df = df.drop(train_df.index)
-test_x = test_df.loc[:, test_df.columns != 'Outcome'].values
-test_y = test_df['Outcome'].values.reshape(-1, 1)
-
-weights = train(train_df, epoch=1)
-
-test_y_hat = predict(test_x, weights)
-print(calc_acc(test_y, test_y_hat))
+if __name__ == '__main__':
+    main()
